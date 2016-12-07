efc4095bbb310406e15b643242d41b7d82fffa31
zhaoyuqi <1064639871@qq.com>
2016-12-07 20:21:44 +0800

    [[ 11 ]]  ---  UIImage 基础类，继承自sprite

--- libzq/ui/UIImage.lua
+++ libzq/ui/UIImage.lua
@@ -8,41 +8,11 @@ local UIImage = class("UIImage", function()
         return cc.Sprite:create()
 end)
 
-
 --------------------------------
 -- UIImage构建函数
 -- @function ctor
-
 function UIImage:ctor()
-
-end
-
-function UIImage:init()
-    self._image_plist = nil
-    self._image_frame = nil
-end
-
-function UIImage:setImage(image)
-    return self:setFrame(nil, image);
-end
-
-function UIImage:setFrame(plist, frame)
-    if self._image_plist == plist and self._image_frame == frame then
-        return
-    end
-
-    local spriteFrame = nil
-    if frame then
-        if plist then
-            spriteFrame =
-        else
-            --todo
-        end
-
-    else
-
-    end
-
+    self:setCascadeOpacityEnabled()
 end
 
 return UIImage
--- libzq/ui/init.lua
+++ libzq/ui/init.lua
@@ -5,7 +5,5 @@
 local CURRENT_MODULE_NAME = ...
 
 
-import()
-require(cc.PACKAGE_NAME .. ".functions")
-require(cc.PACKAGE_NAME .. ".cocos2dx")
+zq.UIImage = import(".UIImage")
 
