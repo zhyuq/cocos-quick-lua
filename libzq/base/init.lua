ccb58730c9467ebd137143d7643763e8c0cc92d6
zhaoyuqi <1064639871@qq.com>
2016-12-09 19:08:53 +0800

    [[ 15 ]]  ---  添加一个button 控件， 未完成

--- libzq/base/init.lua
+++ libzq/base/init.lua
@@ -19,5 +19,11 @@ zq.intToc3b = color.intToc3b
 zq.Int32Max = 2147483647;
 zq.Int32Min = -2147483648;
 
+function fif(condition, if_true, if_false)
+  if condition then return if_true else return if_false end
+end
+
+
+
 
 
--- libzq/ui/init.lua
+++ libzq/ui/init.lua
@@ -7,4 +7,5 @@ local CURRENT_MODULE_NAME = ...
 
 zq.UIImage = import(".UIImage")
 zq.UITouch = import(".UITouch")
+zq.UIButton = import(".UIButton")
 
